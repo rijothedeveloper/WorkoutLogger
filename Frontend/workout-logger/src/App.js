3461d83af57ee57d74e25d8b3d9949dc48ae4e8e
Rijo George <rijogeorge@rijos-mbp.lan>
2022-05-15 18:22:38 -0700

    add react router

--- Frontend/workout-logger/src/App.js
+++ Frontend/workout-logger/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <h1>navigation here</h1>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/workout-logger/src/components/Footer.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return <Link to="/about">About</Link>;
+};
+
+export default Footer;
