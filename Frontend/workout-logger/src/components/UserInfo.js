55bf7934af41485834e98e058dfdaacdc172afce
Rijo George <rijogeorge@rijos-mbp.lan>
2022-05-23 18:05:45 -0700

    user info shows details

--- Frontend/workout-logger/src/components/UserInfo.js
+++ Frontend/workout-logger/src/components/UserInfo.js
@@ -8,20 +8,103 @@ const UserInfo = ({ token }) => {
       if (token) {
         const u = await fetchUserInfo(token);
         setUser(u);
+        setFormData({
+          firstName: u.firstName,
+          lastName: u.lastName,
+          username: u.username,
+          password: "",
+          height: u.height,
+          weight: u.weight,
+        });
       }
     };
     getUserInfo(token);
   }, [token]);
 
+  const [formData, setFormData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    password: "",
+    height: user.height,
+    weight: user.weight,
+  });
+
+  const onChangeForm = (evt) => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h3>{user.firstname}</h3>
-      <h3>{user.lastname}</h3>
-      <h3>{user.username}</h3>
-      <h3>{user.height}</h3>
-      <h3>{user.weight}</h3>
-    </div>
+    <form method="post" onSubmit={onSubmit}>
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={onChangeForm}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={onChangeForm}
+        />
+      </label>
+      <label>
+        User Name:
+        <input type="text" name="username" value={formData.username} />
+      </label>
+      <label>
+        password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={onChangeForm}
+        />
+      </label>
+      <label>
+        Height:
+        <input
+          type="text"
+          name="height"
+          value={formData.height}
+          onChange={onChangeForm}
+        />
+      </label>
+      <label>
+        weight:
+        <input
+          type="text"
+          name="weight"
+          value={formData.weight}
+          onChange={onChangeForm}
+        />
+      </label>
+      <input type="submit" value="Save" />
+    </form>
   );
+
+  // return (
+  //   <div>
+  //     <h3>{user.firstname}</h3>
+  //     <h3>{user.lastname}</h3>
+  //     <h3>{user.username}</h3>
+  //     <h3>{user.height}</h3>
+  //     <h3>{user.weight}</h3>
+  //   </div>
+  // );
 };
 
 export default UserInfo;
