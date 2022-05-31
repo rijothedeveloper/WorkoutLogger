67b39e89b1b4fea26c0b106a71af75f1e2df8bbb
Rijo George <rijogeorge@rijos-mbp.lan>
2022-05-31 10:50:26 -0700

    home page updated

--- Frontend/workout-logger/src/components/Main.js
+++ Frontend/workout-logger/src/components/Main.js
@@ -1,15 +1,38 @@
 import Plans from "./Plans";
 import SavedPlans from "./SavedPlans";
 import Workouts from "./Workouts";
+import UserContext from "../UserContext";
+import { useContext } from "react";
+import NavItem from "./NavItem";
+import fitImg from "../images/fitnessbg.jpeg";
 
 const Main = () => {
-  return (
-    <>
-      <SavedPlans />
-      <Plans />
-      <Workouts />
-    </>
-  );
+  const [user] = useContext(UserContext);
+  function outPut() {
+    if (user.loggedin) {
+      return (
+        <>
+          <SavedPlans />
+          <Plans />
+          <Workouts />
+        </>
+      );
+    } else {
+      return (
+        <div class="home">
+          <div class="homec">
+            <h1>Fitness To Look Cool</h1>
+            <NavItem link="login" />
+            <NavItem link="register" />
+          </div>
+          <div class="homec">
+            <img src={fitImg} class="homec" />
+          </div>
+        </div>
+      );
+    }
+  }
+  return outPut();
 };
 
 export default Main;
