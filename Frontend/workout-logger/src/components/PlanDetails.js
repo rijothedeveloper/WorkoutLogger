f75076019ab59f4e5cd4d5ac211e858160eb5cd4
rijothedeveloper <rijothedeveloper@gmail.com>
2022-06-13 09:57:57 -0700

    menu imp for mobile

--- Frontend/workout-logger/src/components/PlanDetails.js
+++ Frontend/workout-logger/src/components/PlanDetails.js
@@ -10,37 +10,22 @@ const PlanDetails = () => {
   const [plan, setPlan] = useState({});
   const navigate = useNavigate();
 
-  if (!user.token) {
-    navigate("/");
-  }
-
   useEffect(() => {
     const fetchPlanDetails = async () => {
-      const planDetails = await fetchPlan(user.token, planId);
-      setPlan(planDetails);
+      if (user.token) {
+        const planDetails = await fetchPlan(user.token, planId);
+        setPlan(planDetails);
+      }
     };
-    if (user.token) fetchPlanDetails();
-  }, [user.token, planId]);
-  let workElem;
-  if (plan.workouts) {
-    workElem = plan.workouts.map((e) => <h4>{e.name}</h4>);
-  }
+    fetchPlanDetails();
+  }, []);
 
   return (
     <div>
       <h2>PlanDetails</h2>
       <h3>name: {plan.name} </h3>
       <h3>notes: {plan.notes} </h3>
-      <h3>Sunday: {plan.sun ? "Yes" : "No"}</h3>
-      <h3>Monday: {plan.mon ? "Yes" : "No"}</h3>
-      <h3>Tuesday: {plan.tue ? "Yes" : "No"}</h3>
-      <h3>Wednesday: {plan.wed ? "Yes" : "No"}</h3>
-      <h3>Thursday: {plan.thu ? "Yes" : "No"}</h3>
-      <h3>Friday: {plan.fri ? "Yes" : "No"}</h3>
-      <h3>Saturday: {plan.sat ? "Yes" : "No"}</h3>
       <h3>workouts</h3>
-
-      <PlansInfo plan={workElem} />
     </div>
   );
 };
--- Frontend/workout-logger/src/components/PlansInfo.js
+++ Frontend/workout-logger/src/components/PlansInfo.js
@@ -1,6 +1,7 @@
 import Plan from "./Plan";
 
 const PlansInfo = ({ plans }) => {
+  if (!plans) return;
   const planElements = plans.map((e) => <Plan plan={e} key={e.id} />);
 
   return <div className="card-raw">{planElements}</div>;
