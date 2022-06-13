4d602223ea4a35eca39844b1682eaa9a97178285
rijothedeveloper <rijothedeveloper@gmail.com>
2022-06-13 10:54:57 -0700

    hamburger done

--- Frontend/workout-logger/src/components/Navigation.js
+++ Frontend/workout-logger/src/components/Navigation.js
@@ -2,10 +2,19 @@ import NavItem from "./NavItem";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 import UserContext from "../UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 const Navigation = ({ onLogout }) => {
   const [user] = useContext(UserContext);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = (event) => {
+    setNavbarOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
   let navSec;
   if (!user.loggedin) {
     navSec = (
@@ -21,41 +30,48 @@ const Navigation = ({ onLogout }) => {
   } else {
     navSec = (
       <>
-        <li class="logo">
+        <li class="logo" onClick={() => closeMenu()}>
           <Link to="/">
             <img src={logo} />
           </Link>
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="myplans" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="mybookmarkedplans" title="Bookmarked Plans" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="plans" title="All Plans" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="workouts" title="All Workouts" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="newWorkout" title="Create Workout" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="newPlan" title="create Plan" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link="logout" />
         </li>
-        <li>
+        <li onClick={() => closeMenu()}>
           <NavItem link={"user/" + user.username} title={user.username} />
         </li>
       </>
     );
   }
   return (
-    <nav>
-      <ul>{navSec}</ul>
+    <nav className="navBar">
+      <button onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+        ) : (
+          <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>{navSec}</ul>
     </nav>
   );
 };
